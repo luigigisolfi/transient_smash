ab72400d782df3fc13fddbd556a8ddf5f5aff263
JJD333 <jackdavey3@hotmail.co.uk>
2025-09-10 16:15:36 +0100

    Added test for numpy array values in the simple model.

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,11 +1,16 @@
 from transient_smash.models.model import Model,SimpleModel
 import unittest
+import numpy as np
 
 class TestSimpleModel(unittest.TestCase):
     
     def test_simulator(self):
+        """Test the simple model and its simulator function."""
         simple_model = SimpleModel()
+
+        ### Test in the case of an integer input or an np array
         self.assertEqual(simple_model.simulator(x=1,a=1,b=1),2)
+        self.assertTrue((simple_model.simulator(x=np.array([1,2,3]),a=1,b=1)==np.array([2,3,4])).all())
 
 if __name__=='__main__':
     unittest.main()
