34a50d5f0de5d7b8c6af914ac0fd1c47c9a5af91
Emma Godden <emma.godden@physics.ox.ac.uk>
2025-09-11 14:35:51 +0100

    Adding tests for set input data and simulator methods.

--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -14,10 +14,28 @@ class TestSimpleModel(unittest.TestCase):
     def test_evaluate(self):
         """Test the simple model and its simulator function."""
         simple_model = SimpleModel()
-
         ### Test in the case of an integer input or an np array
         self.assertEqual(simple_model.evaluate(x=1,a=1,b=1),2)
         self.assertTrue((simple_model.evaluate(x=np.array([1,2,3]),a=1,b=1)==np.array([2,3,4])).all())
+
+    def test_set_input_data(self):
+        """Test setting input data for the model."""
+        simple_model = SimpleModel()
+        x_data = np.array([0, 1, 2, 3])
+        simple_model.set_input_data(x_data)
+        self.assertTrue((simple_model.x == x_data).all())
+
+    def test_simulator(self):
+        """Test the simple model and its simulator function."""
+        simple_model = SimpleModel()
+        a = 1
+        b = 1
+        ### Test that simulator raises an error if input data not set
+        with self.assertRaises(ValueError):
+            simple_model.simulator(a,b)
+        ### Test that simulator works correctly after setting input data
+        simple_model.set_input_data(np.array([1,2,3]))
+        self.assertTrue((simple_model.simulator(a,b)==np.array([2,3,4])).all())
     
 class TestNoisySimpleModel(unittest.TestCase):
 
