060be87372beb8372c128a7155955aff279e440d
Emma Godden <emma.godden@physics.ox.ac.uk>
2025-09-12 10:20:08 +0100

    Fixing linting tests.

--- src/transient_smash/models/model.py
+++ src/transient_smash/models/model.py
@@ -8,106 +8,119 @@ import numpy as np
 
 
 class Model(ABC):
-
+    """Abstract base class for models."""
     @abstractmethod
-    def evaluate(self, x, *args):
+    def evaluate(self, x: np.ndarray, *args: np.ndarray) -> np.ndarray:
         """Evaluate the model given input data and model parameters.
-        
+
         Args:
             x: Input data for the model (i.e. time points at which to simulate).
             *args: Additional arguments for the evaluation containing model parameters.
 
         Returns:
-            Output of the model.
-            
+            Model output evaluated at the given input data.
+
         """
         ...
 
 
     def simulator(self, *args: np.ndarray) -> np.ndarray:
         """Generate simulated data given the pre-set input data and model parameters.
-        
+
         Args:
             *args: Additional arguments for the simulation containing model parameters.
 
         Returns:
             Simulated output of the model.
-            
+
         """
         # Check if input data has been set
         if not hasattr(self, 'x'):
-            raise ValueError("Input data 'x' has not been set. Please use the 'set_input_data' method to set it before calling the simulator.")
+            err_str = "Input data 'x' has not been set. Please use the " \
+            "'set_input_data' method to set it before calling the simulator."
+            raise Exception(err_str)
         return self.evaluate(self.x, *args)
 
-    def set_input_data(self, x: np.ndarray):
+    def set_input_data(self, x: np.ndarray) -> None:
         """Set input data for the model.
-        
+
         Args:
             x: Input data for the model (i.e. time points at which to simulate).
 
         Returns:
             None
-            
+
         """
         self.x = x
         return
 
     def get_sbi_simulator(self) -> callable:
-        """Get a simulator function compatible with the sbi package.
-        """
+        """Get a simulator function compatible with the sbi package."""
         priors = self.get_sbi_priors()
         is_numpy_simulator = True
-        sbi_simulator = process_simulator(self.simulator, priors, is_numpy_simulator)
-        return sbi_simulator
+        return process_simulator(self.simulator, priors, is_numpy_simulator)
 
 
-    def set_priors(self, priors):
+    def set_priors(self, priors) -> None:
         """Set prior distributions for model parameters.
-        
+
         Args:
-            prior_info: A dictionary containing prior distribution information for each parameter.
+            prior_info: A dictionary containing prior distribution information 
+            for each parameter.
 
         Returns:
             None
-            
+
         """
-        num_dims = len(priors.keys())
         priors_list = []
-        for param in priors.keys():
+        for param in priors:
             dist = priors[param][0]
             if dist.lower()=='uniform':
 
                 priors_list.append(
-                        Uniform(torch.tensor([priors[param][1]]),torch.tensor([priors[param][2]])))
+                        Uniform(torch.tensor([priors[param][1]]),
+                                torch.tensor([priors[param][2]])))
             elif dist.lower()=='normal':
                 priors_list.append(
-                        Normal(torch.tensor([priors[param][1]]),torch.tensor([priors[param][2]])))
+                        Normal(torch.tensor([priors[param][1]]),
+                               torch.tensor([priors[param][2]])))
             else:
-                raise ValueError(f"Invalid distribution selected: {dist}")
+                err_str = f"Invalid distribution selected: {dist}. " \
+                           f"Please choose either 'uniform' or 'normal'."
+                raise Exception(err_str)
         self.priors = MultipleIndependent(priors_list)
 
-    def get_sbi_priors(self):
+    def get_sbi_priors(self) -> MultipleIndependent:
         """Get prior distributions for model parameters.
-        
+
         Returns:
-            PyTorch distribution-like priors for each parameter.
-            
+            Distribution-like priors for each parameter.
+
         """
         if not hasattr(self,'priors'):
-            raise ValueError("Priors have not been set. Please use the 'set_priors' method to set them before calling this method.")
+            err_str = "Priors have not been set. Please use the " \
+            "'set_priors' method to set them before calling this method."
+            raise Exception(err_str)
 
         return self.priors
             
 
 
 class SimpleModel(Model):
-    def evaluate(self, x, params):
+    """A simple linear model: y = a * x + b."""
+    def evaluate(self, x: np.ndarray, params: np.ndarray) -> np.ndarray:
         """
-        x: np.ndarray of shape (M,)
-        params: np.ndarray of shape (N, 2)
-        Returns: shape (N, M)
+        Evaluate the simple linear model.
+
+        Args:
+            x: np.ndarray of shape (M,)
+            params: np.ndarray of shape (N, 2)
+
+        Returns:
+            np.ndarray of shape (N, M)
         """
-        params = np.array(params)  # if you want to handle both np/torch uniformly
+
+        params = np.array(params)  # handle np/torch uniformly
     
         if params.ndim == 1:  # single (a, b)
             a, b = params
@@ -119,13 +132,24 @@ class SimpleModel(Model):
             return a * x[None, :] + b
         
         else:
-            raise ValueError(f"Unexpected params shape: {params.shape}")
-    
-class SimpleModelWithNoise(Model):
+            err_str = f"Unexpected params shape: {params.shape}"
+            raise Exception(err_str)
 
-    def evaluate(self, x, params):
-        """A simple linear model: y = a * x + b."""
-        params = np.array(params)  # if you want to handle both np/torch uniformly
+class SimpleModelWithNoise(Model):
+    """A simple linear model with Gaussian noise: y = a * x + b + noise."""
+
+    def evaluate(self, x: np.ndarray, params: np.ndarray) -> np.ndarray:
+        """Evaluate the simple linear model with noise.
+
+        Args:
+            x: Input data for the model.
+            params: Model parameters (a, b, noise_mean, noise_std).
+
+        Returns:
+            Model output evaluated at the given input data plus noise.
+        """
+
+        params = np.array(params)  # handle both np/torch uniformly
         if params.ndim == 1:  # single (a, b, noise_mean, noise_std)
             a, b, noise_mean, noise_std = params
         elif params.ndim == 2:  # multiple (a, b, noise_mean, noise_std) pairs
@@ -134,13 +158,15 @@ class SimpleModelWithNoise(Model):
             noise_mean = params[:, 2][:, None]  # shape (N, 1)
             noise_std = params[:, 3][:, None]
         else:
-            raise ValueError(f"Unexpected params shape: {params.shape}")
+            err_str = f"Unexpected params shape: {params.shape}"
+            raise Exception(err_str)
 
         return a * x + b + self.noise(x, noise_mean, noise_std)
 
-    def noise(self, x, noise_mean, noise_std):
+    def noise(self, x: np.ndarray, noise_mean: np.ndarray, 
+              noise_std: np.ndarray) -> np.ndarray:
         """Simulate the noise to apply to the model.
-        
+
         Args:
             x: Input data for the model.
             noise_mean: Mean of the noise distribution.
@@ -149,27 +175,41 @@ class SimpleModelWithNoise(Model):
         return norm.rvs(loc=noise_mean, scale=noise_std, size=len(x))
 
 class SinusoidalModelWithNoise(Model):
-    
-    def evaluate(self, x, params):
-        """A sinusoidal model: y = A * sin(2 * pi * f * x + phi) + b."""
-        params = np.array(params)  # if you want to handle both np/torch uniformly
+    """A sinusoidal model with Gaussian noise: y = A * sin(2 * pi * f * x +
+      phi) + b + noise."""
+
+    def evaluate(self, x: np.ndarray, params: np.ndarray) -> np.ndarray:
+        """Evaluate the sinusoidal model with noise.
+
+        Args:
+            x: Input data for the model.
+            params: Model parameters (A, f, phi, b, noise_mean, noise_std).
+
+        Returns:
+            Model output evaluated at the given input data plus noise.
+        """
+
+        params = np.array(params)  # handle both np/torch uniformly
         if params.ndim == 1:  # single (A, f, phi, b, noise_mean, noise_std)
-            A, f, phi, b, noise_mean, noise_std = params
-        elif params.ndim == 2:  # multiple (A, f, phi, b, noise_mean, noise_std) pairs
-            A = params[:, 0][:, None]  # shape (N, 1)
+            a, f, phi, b, noise_mean, noise_std = params
+        elif params.ndim == 2:  # (A, f, phi, b, noise_mean, noise_std) pairs
+            a = params[:, 0][:, None]  # shape (N, 1)
             f = params[:, 1][:, None]  # shape (N, 1)
             phi = params[:, 2][:, None]  # shape (N, 1)
             b = params[:, 3][:, None]  # shape (N, 1)
             noise_mean = params[:, 4][:, None]  # shape (N, 1)
             noise_std = params[:, 5][:, None]
         else:
-            raise ValueError(f"Unexpected params shape: {params.shape}")
+            err_str = f"Unexpected params shape: {params.shape}"
+            raise Exception(err_str)
 
-        return A * np.sin(2 * np.pi * f * x + phi) + b + self.noise(x, noise_mean, noise_std)
+        return a * np.sin(2 * np.pi * f * x + phi) + b + \
+        self.noise(x, noise_mean, noise_std)
 
-    def noise(self, x, noise_mean, noise_std):
+    def noise(self, x: np.ndarray, noise_mean: np.ndarray, 
+              noise_std: np.ndarray) -> np.ndarray:
         """Simulate the noise to apply to the model.
-        
+
         Args:
             x: Input data for the model.
             noise_mean: Mean of the noise distribution.
--- tests/models/test_model.py
+++ tests/models/test_model.py
@@ -1,109 +1,115 @@
-from transient_smash.models.model import *
+from transient_smash.models.model import Model, SimpleModel, \
+SimpleModelWithNoise, SinusoidalModelWithNoise
 import unittest
 import numpy as np
 from scipy.stats import norm
+import pytest
+import torch
+from torch.distributions import Uniform, Normal
 
 class TestModel(unittest.TestCase):
-    def test_abstract_model(self):
+    """Test the abstract Model class."""
+    def test_abstract_model(self) -> None:
         """Test that the abstract Model class cannot be instantiated."""
-        with self.assertRaises(TypeError):
-            model = Model()  # This should raise a TypeError since Model is abstract
+        with pytest.raises(TypeError):
+            Model()  # This should raise a TypeError since Model is abstract
 
 class TestSimpleModel(unittest.TestCase):
-    
-    def test_evaluate(self):
+    """Test the SimpleModel class."""
+    def test_evaluate(self) -> None:
         """Test the simple model and its simulator function."""
         simple_model = SimpleModel()
         ### Test in the case of an integer input or an np array
-        self.assertEqual(simple_model.evaluate(x=1,params=[1,1]),2)
-        self.assertTrue((simple_model.evaluate(x=np.array([1,2,3]),params=[1,1])==np.array([2,3,4])).all())
+        assert simple_model.evaluate(x=1,params=[1,1]) == 2
+        assert (simple_model.evaluate(x=np.array([1,2,3]),params=[1,1]) == 
+                np.array([2,3,4])).all()
 
-    def test_set_input_data(self):
+    def test_set_input_data(self) -> None:
         """Test setting input data for the model."""
         simple_model = SimpleModel()
         x_data = np.array([0, 1, 2, 3])
         simple_model.set_input_data(x_data)
-        self.assertTrue((simple_model.x == x_data).all())
+        assert (simple_model.x == x_data).all()
 
-    def test_simulator(self):
+    def test_simulator(self) -> None:
         """Test the simple model and its simulator function."""
         simple_model = SimpleModel()
         params = torch.tensor([[1.0, 1.0]])
         ### Test that simulator raises an error if input data not set
-        with self.assertRaises(ValueError):
+        with pytest.raises(Exception):
             simple_model.simulator(params)
         ### Test that simulator works correctly after setting input data
         simple_model.set_input_data(np.array([1,2,3]))
-        self.assertTrue((simple_model.simulator(params)==np.array([2,3,4])).all())
+        assert (simple_model.simulator(params) == np.array([2,3,4])).all()
 
-    def test_set_priors(self):
-        from torch.distributions import Uniform, Normal
+    def test_set_priors(self) -> None:
         """Test setting prior distributions for model parameters."""
         simple_model = SimpleModel()
         # Test setting a prior which is not recognized raises an error
-        with self.assertRaises(ValueError):
+        with pytest.raises(Exception):
             simple_model.set_priors({'a': ('unknown', 0., 1.)})
         # Now set valid priors and test
         priors = {'a': ('uniform', 0., 2.), 'b': ('normal', 0., 2.)}
         simple_model.set_priors(priors)
         # Test that the priors are a list of length 2 (for a and b)
-        self.assertEqual(len(simple_model.priors.dists), 2)
+        assert len(simple_model.priors.dists) == 2
         # Test that the priors are the expected type
-        self.assertIsInstance(simple_model.priors.dists[0], Uniform)
-        self.assertIsInstance(simple_model.priors.dists[1], Normal)
+        assert isinstance(simple_model.priors.dists[0], Uniform)
+        assert isinstance(simple_model.priors.dists[1], Normal)
         # Test that the priors have the expected parameters
-        self.assertEqual(simple_model.priors.dists[0].low, 0.)
-        self.assertEqual(simple_model.priors.dists[0].high, 2.)
-        self.assertEqual(simple_model.priors.dists[1].loc, 0.)
-        self.assertEqual(simple_model.priors.dists[1].scale, 2.)
+        assert simple_model.priors.dists[0].low == 0.
+        assert simple_model.priors.dists[0].high == 2.
+        assert simple_model.priors.dists[1].loc == 0.
+        assert simple_model.priors.dists[1].scale == 2.
 
-    def test_get_sbi_priors(self):
-        from torch.distributions import Uniform, Normal
+    def test_get_sbi_priors(self) -> None:
         """Test getting sbi-compatible prior distributions."""
         simple_model = SimpleModel()
         # Test that getting priors without setting them raises an error
-        with self.assertRaises(ValueError):
+        with pytest.raises(Exception):
             simple_model.get_sbi_priors()
         # Now set priors and test getting them
         priors = {'a': ('uniform', 0., 2.), 'b': ('normal', 0., 2.)}
         simple_model.set_priors(priors)
         sbi_priors = simple_model.get_sbi_priors()
         # Test that the returned priors are a list of length 2 (for a and b)
-        self.assertEqual(len(sbi_priors.dists), 2)
+        assert len(sbi_priors.dists) == 2
         # Test that the priors are the expected type
-        self.assertIsInstance(sbi_priors.dists[0], Uniform)
-        self.assertIsInstance(sbi_priors.dists[1], Normal)
+        assert isinstance(sbi_priors.dists[0], Uniform)
+        assert isinstance(sbi_priors.dists[1], Normal)
         # Test that the priors have the expected parameters
-        self.assertEqual(sbi_priors.dists[0].low, 0.)
-        self.assertEqual(sbi_priors.dists[0].high, 2.)
-        self.assertEqual(sbi_priors.dists[1].loc, 0.)
-        self.assertEqual(sbi_priors.dists[1].scale, 2.)
+        assert sbi_priors.dists[0].low == 0.
+        assert sbi_priors.dists[0].high == 2.
+        assert sbi_priors.dists[1].loc == 0.
+        assert sbi_priors.dists[1].scale == 2.
 
-    def test_sample_prior(self):
+    def test_sample_prior(self) -> None:
         """Test sampling from the prior distributions."""
         simple_model = SimpleModel()
         prior_info = {'a': ('uniform', 0., 2.), 'b': ('normal', 0., 2.)}
         simple_model.set_priors(prior_info)
         priors = simple_model.get_sbi_priors()
         samples = priors.sample((5,))
-        # Test that the samples have the expected shape (5 samples of 2 parameters)
-        self.assertEqual(samples.shape, (5, 2))
+        # Test that the samples have the expected shape
+        assert samples.shape == (5, 2)
 
-    def test_get_sbi_simulator(self):
+    def test_get_sbi_simulator(self) -> None:
         """Test getting an sbi-compatible simulator function."""
         simple_model = SimpleModel()
-        simple_model.set_priors({'a': ('uniform', 0., 2.), 'b': ('uniform', 0., 2.)})
+        simple_model.set_priors({'a': ('uniform', 0., 2.), 
+                                 'b': ('uniform', 0., 2.)})
         x = np.array([1, 2, 3])
         simple_model.set_input_data(x)
         sbi_simulator = simple_model.get_sbi_simulator()
         # Test that the returned simulator is callable
-        self.assertTrue(callable(sbi_simulator))
+        assert callable(sbi_simulator)
         # Test that the simulator produces expected output for given parameters
         y = sbi_simulator(torch.tensor([[1.0, 1.0]]))
-        self.assertIsInstance(y, torch.Tensor)
+        assert isinstance(y, torch.Tensor)
 
 class TestSimpleModelWithNoise(unittest.TestCase):
-    def test_evaluate_with_noise(self):
+    """Test the SimpleModelWithNoise class."""
+    def test_evaluate_with_noise(self) -> None:
         """Test the simple model with noise and its simulator function."""
         model = SimpleModelWithNoise()
         # Test evaluate method with specific noise parameters
@@ -111,58 +117,60 @@ class TestSimpleModelWithNoise(unittest.TestCase):
         params = [1, 1, 0, 1]  # a=1, b=1, noise_mean=0, noise_std=1
         result = model.evaluate(x=np.array([1, 2, 3]), params=params)
         expected = np.array([2, 3, 4]) + norm.rvs(loc=0, scale=1, size=3, random_state=42)
-        self.assertTrue(np.allclose(result, expected))
+        assert np.allclose(result, expected)
 
-    def test_simulator_with_noise(self):
+    def test_simulator_with_noise(self) -> None:
         """Test the simulator method of the simple model with noise."""
         model = SimpleModelWithNoise()
         model.set_input_data(np.array([1, 2, 3]))
         np.random.seed(42)  # For reproducibility
-        params = torch.tensor([[1.0, 1.0, 0.0, 1.0]])  # a=1, b=1, noise_mean=0, noise_std=1
+        params = torch.tensor([[1.0, 1.0, 0.0, 1.0]])  
         result = model.simulator(params)
-        expected = np.array([2, 3, 4]) + norm.rvs(loc=0, scale=1, size=3, random_state=42)
-        self.assertTrue(np.allclose(result, expected))
+        expected = np.array([2, 3, 4]) + \
+            norm.rvs(loc=0, scale=1, size=3, random_state=42)
+        assert np.allclose(result, expected)
 
-    def test_noise_generation(self):
+    def test_noise_generation(self) -> None:
         """Test the noise generation method."""
         model = SimpleModelWithNoise()
         np.random.seed(42)
         noise = model.noise(x=np.array([1, 2, 3]), noise_mean=0, noise_std=1)
         expected = norm.rvs(loc=0, scale=1, size=3, random_state=42)
-        self.assertTrue(np.allclose(noise, expected))
-        self.assertEqual(len(noise), 3)  # Ensure noise length matches input length
+        assert np.allclose(noise, expected)
+        assert len(noise) == 3  # Ensure noise length matches input length
 
 class TestSinusoidalModelWithNoise(unittest.TestCase):
-    def test_evaluate_sinusoidal(self):
+    """Test the SinusoidalModelWithNoise class."""
+    def test_evaluate_sinusoidal(self) -> None:
         """Test the sinusoidal model with noise and its simulator function."""
         model = SinusoidalModelWithNoise()
         # Test evaluate method with specific noise parameters
         np.random.seed(42)  # For reproducibility
-        params = [1, 2, 0, 0, 0, 1]  # amplitude=1, frequency=2, phase=0, offset=0, noise_mean=0, noise_std=1
+        params = [1, 2, 0, 0, 0, 1]  
         result = model.evaluate(x=np.array([0, 1, 2]), params=params)
         expected = 1 * np.sin(2 * np.pi * 2 * np.array([0, 1, 2]) + 0) + 0
         expected += norm.rvs(loc=0, scale=1, size=3, random_state=42)
-        self.assertTrue(np.allclose(result, expected))
+        assert np.allclose(result, expected)
 
-    def test_simulator_sinusoidal(self):
+    def test_simulator_sinusoidal(self) -> None:
         """Test the simulator method of the sinusoidal model with noise."""
         model = SinusoidalModelWithNoise()
         model.set_input_data(np.array([0, 1, 2]))
         np.random.seed(42)  # For reproducibility
-        params = torch.tensor([[1.0, 2.0, 0.0, 0.0, 0.0, 1.0]])  # amplitude=1, frequency=2, phase=0, offset=0, noise_mean=0, noise_std=1
+        params = torch.tensor([[1.0, 2.0, 0.0, 0.0, 0.0, 1.0]])  
         result = model.simulator(params)
         expected = 1 * np.sin(2 * np.pi * 2 * np.array([0, 1, 2]) + 0) + 0
         expected += norm.rvs(loc=0, scale=1, size=3, random_state=42)
-        self.assertTrue(np.allclose(result, expected))
+        assert np.allclose(result, expected)
 
-    def test_noise_generation_sinusoidal(self):
+    def test_noise_generation_sinusoidal(self) -> None:
         """Test the noise generation method in the sinusoidal model."""
         model = SinusoidalModelWithNoise()
         np.random.seed(42)
         noise = model.noise(x=np.array([0, 1, 2]), noise_mean=0, noise_std=1)
         expected = norm.rvs(loc=0, scale=1, size=3, random_state=42)
-        self.assertTrue(np.allclose(noise, expected))
-        self.assertEqual(len(noise), 3)  # Ensure noise length matches input length
+        assert np.allclose(noise, expected)
+        assert len(noise) == 3  # Ensure noise length matches input length
 
 
 if __name__=='__main__':
